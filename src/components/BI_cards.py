e6b132be99f212b0da8ad064ab34faf7ef0bd7ab
DongchunChen <dc.chen2749@gmail.com>
2025-03-14 12:38:01 -0700

    modify layout for title and sidebar

--- src/components/BI_cards.py
+++ src/components/BI_cards.py
@@ -3,53 +3,84 @@ from dash import Dash, Output, Input, html
 import dash_bootstrap_components as dbc
 
 def update_cards(df, state, county):
-    if not county:
-        sellability_empty = [
-            dbc.CardHeader("Sellability index"),
-            dbc.CardBody("[Select a county]"),
-            dbc.CardFooter("Percentile - median income", style={'fontSize': '10px'})
-        ]
-        growth_empty = [
-            dbc.CardHeader("Growth index"),
-            dbc.CardBody("[Select a county]"),
-            dbc.CardFooter("Percentile - yearly Microbusiness growth", style={'fontSize': '10px'})
-        ]
-        hireability_empty = [
-            dbc.CardHeader("Hireability index"),
-            dbc.CardBody("[Select a county]"),
-            dbc.CardFooter("Percentile - pop. % with bachelors degree", style={'fontSize': '10px'})
-        ]
-        return sellability_empty, growth_empty, hireability_empty
+    """Generate styled metric cards with clean background & no border."""
     
-    #creating df of filtered counties at latest date
+    card_style = {
+        'backgroundColor': '#D7EBF6', 
+         'borderRadius': '5px',  
+        'border': 'none',  
+        'boxShadow': 'none'
+    }
+
+    header_footer_style = {
+        'backgroundColor': '#D7EBF6'
+    }
+
+    if not county:
+        sellability_empty = dbc.Card(
+            [
+                dbc.CardHeader("Sellability index", style=header_footer_style),
+                dbc.CardBody(html.P("[Please select a county]", style={'fontSize': '15px'})),
+                dbc.CardFooter("Percentile - median income", style={**header_footer_style, 'fontSize': '11px'})
+            ],
+            style=card_style  
+        )
+
+        growth_empty = dbc.Card(
+            [
+                dbc.CardHeader("Growth index", style=header_footer_style),
+                dbc.CardBody(html.P("[Please select a county]", style={'fontSize': '15px'})),
+                dbc.CardFooter("Percentile - yearly Microbusiness growth", style={**header_footer_style, 'fontSize': '11px'})
+            ],
+            style=card_style  
+        )
+
+        hireability_empty = dbc.Card(
+            [
+                dbc.CardHeader("Hireability index", style=header_footer_style),
+                dbc.CardBody(html.P("[Please select a county]", style={'fontSize': '15px'})),
+                dbc.CardFooter("Percentile - pop. % with bachelors degree", style={**header_footer_style, 'fontSize': '11px'})
+            ],
+            style=card_style 
+        )
+
+        return sellability_empty, growth_empty, hireability_empty
+
+    # Creating df of filtered counties at latest date
     latest_date = "2022-10-01"
     condition = "state in @state & county in @county & first_day_of_month == @latest_date"
     filtered_df = df.query(condition)
 
-    #create index paragraph lists and populate with filtered df
-    sell_list = []
-    growth_list = []
-    hire_list = []
-    for i in range(len(filtered_df)):
-        county = filtered_df.iloc[i]
-        sell_list.append(html.P(f"{county['county']} : {county['sellability_index']}%"))
-        growth_list.append(html.P(f"{county['county']} : {county['growth_index']}%"))
-        hire_list.append(html.P(f"{county['county']} : {county['hireability_index']}%"))
+    # Create index paragraph lists and populate with filtered df
+    sell_list = [html.P(f"{county['county']} : {county['sellability_index']}%") for _, county in filtered_df.iterrows()]
+    growth_list = [html.P(f"{county['county']} : {county['growth_index']}%") for _, county in filtered_df.iterrows()]
+    hire_list = [html.P(f"{county['county']} : {county['hireability_index']}%") for _, county in filtered_df.iterrows()]
 
+    sellability_card = dbc.Card(
+        [
+            dbc.CardHeader("Sellability index", style=header_footer_style),
+            dbc.CardBody(sell_list, style={'fontSize': '14px'}),
+            dbc.CardFooter("Percentile - median income", style={**header_footer_style, 'fontSize': '11px'})
+        ],
+        style=card_style  
+    )
 
-    sellability_card = [
-        dbc.CardHeader("Sellability index"),
-        dbc.CardBody(sell_list),
-        dbc.CardFooter("Percentile - median income", style={'fontSize': '10px'})
-    ]
-    growth_card = [
-        dbc.CardHeader("Growth index"),
-        dbc.CardBody(growth_list),
-        dbc.CardFooter("Percentile - yearly Microbusiness growth", style={'fontSize': '10px'})
-    ]
-    hireability_card = [
-        dbc.CardHeader("Hireability index"),
-        dbc.CardBody(hire_list),
-        dbc.CardFooter("Percentile - pop. % with bachelors degree", style={'fontSize': '10px'})
-    ]
+    growth_card = dbc.Card(
+        [
+            dbc.CardHeader("Growth index", style=header_footer_style),
+            dbc.CardBody(growth_list, style={'fontSize': '14px'}),
+            dbc.CardFooter("Percentile - yearly Microbusiness growth", style={**header_footer_style, 'fontSize': '11px'})
+        ],
+        style=card_style 
+    )
+
+    hireability_card = dbc.Card(
+        [
+            dbc.CardHeader("Hireability index", style=header_footer_style),
+            dbc.CardBody(hire_list, style={'fontSize': '14px'}),
+            dbc.CardFooter("Percentile - pop. % with bachelors degree", style={**header_footer_style, 'fontSize': '11px'})
+        ],
+        style=card_style  
+    )
+
     return sellability_card, growth_card, hireability_card
--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -4,30 +4,37 @@ import dash_bootstrap_components as dbc
 def create_sidebar(total_microbusinesses, weighted_microbusiness_density, median_income):
     """Creates sidebar metrics for the dashboard."""
     return html.Div([
-        html.H4("USA-wide Metrics", style={'textAlign': 'center', 'fontSize': '20px', 'marginBottom': '25px'}),
+        html.H4("USA-wide Metrics", style={'textAlign': 'center', 'fontSize': '20px', 'marginTop': '20px', 'marginBottom': '25px'}),
         html.Div([
             html.H6("Total Microbusinesses", style={'marginBottom': '7px', 'fontSize': '17px'}),
             html.Hr(style={'border': '1px solid #AAC8E4', 'width': '80%', 'margin': '20px auto'}),
             html.P(f"{total_microbusinesses:,.0f}", style={'fontSize': '17px', 'fontWeight': 'bold', 'marginTop': '5px'})
-        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'marginBottom': '20px'}),
+        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'margin': '0 auto 20px', 'width': '90%'}),
         html.Div([
             html.H6("Avg. Microbusiness Density", style={'marginBottom': '7px', 'fontSize': '17px'}),
+            html.P("(Microbusinesses per 100 people)", style={'fontSize': '12px', 'color': 'black', 'marginBottom': '5px'}),
             html.Hr(style={'border': '1px solid #AAC8E4', 'width': '80%', 'margin': '20px auto'}),
             html.P(f"{weighted_microbusiness_density:.2f}", style={'fontSize': '17px', 'fontWeight': 'bold', 'marginTop': '5px'})
-        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'marginBottom': '20px'}),
+        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'margin': '0 auto 20px', 'width': '90%'}),
         html.Div([
             html.H6("Median Household Income", style={'marginBottom': '7px', 'fontSize': '17px'}),
             html.Hr(style={'border': '1px solid #AAC8E4', 'width': '80%', 'margin': '20px auto'}),
             html.P(f"${median_income:,.0f}", style={'fontSize': '17px', 'fontWeight': 'bold', 'marginTop': '5px'})
-        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'marginBottom': '20px'}),
-    ], style={'border': '2px solid black', 'padding': '15px', 'borderRadius': '10px', 'width': '90%'})
-
+        ], style={'textAlign': 'center', 'backgroundColor': '#D7EBF6', 'padding': '15px', 'borderRadius': '10px', 'margin': '0 auto 20px', 'width': '90%'}),
+    ], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'width': '100%'})
 
 def create_county_sidebar(card_sellability, card_growth, card_hireability):
     return html.Div([
-        html.H4("County Business Indices", style={'textAlign': 'center', 'fontSize': '20px', 'marginBottom': '25px'}),
-        card_sellability,
-        card_growth,
-        card_hireability,
-        html.H6("Business indices measure how the selected county performs in key metrics, compared to all counties in the US", style={'textAlign': 'center', 'marginBottom': '7px', 'fontSize': '12px'}),
-    ],style={'border': '2px solid black', 'padding': '15px', 'borderRadius': '10px', 'width': '90%'})
+        html.H4("County Business Indices", style={'textAlign': 'center', 'fontSize': '20px', 'marginTop': '20px'}),
+
+        # Moved description below the title
+        html.P(
+            "Business indices measure how the selected county performs in key metrics, compared to all counties in the US.",
+            style={'textAlign': 'left', 'marginBottom': '20px', 'fontSize': '13px', 'color': "#4A4A4A", 'padding-left': '12px', 'padding-right': '12px'}
+        ),
+
+        # Adding spacing between cards
+        html.Div(card_sellability, style={'padding-left': '12px', 'padding-right': '12px'}),
+        html.Div(card_growth, style={'padding-left': '12px', 'padding-right': '12px', 'marginTop': '15px'}),
+        html.Div(card_hireability, style={'padding-left': '12px', 'padding-right': '12px', 'marginTop': '15px'}),
+    ])
